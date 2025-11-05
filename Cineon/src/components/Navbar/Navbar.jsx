868c60e99552aa42622a00fa19ab0da95bed44da
xcuong-512 <xcuong286@gmail.com>
2025-11-05 14:02:38 +0700

    Update Navbar.jsx

--- Cineon/src/components/Navbar/Navbar.jsx
+++ Cineon/src/components/Navbar/Navbar.jsx
@@ -12,15 +12,23 @@ const Navbar = () => {
     const navRef = useRef();
 
     useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY >= 80) {
-                navRef.current.classList.add('nav-dark')
+        // Khai báo hàm xử lý scroll
+        const handleScroll = () => {
+            if (navRef.current) {
+                if (window.scrollY >= 80) {
+                    navRef.current.classList.add('nav-dark');
+                } else {
+                    navRef.current.classList.remove('nav-dark');
+                }
             }
-            else {
-                navRef.current.classList.remove('nav-dark')
-            }
-        })
-    }, [])
+        };
+
+        // Thêm event listener
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div ref={navRef} className='navbar'>
